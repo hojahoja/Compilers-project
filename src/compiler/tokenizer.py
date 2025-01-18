6fe91ce2daec1d0261fa871722fcf4b4f50245c5
hojahoja <5722145+hojahoja@users.noreply.github.com>
2025-01-18 16:47:23 +0200

    Tokenizer task 3
    - Operators
    - Punctuation
    - Single line and multiline comments
    - Tests for the above

--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -19,25 +19,38 @@ class Token:
 
 def tokenize(source_code: str) -> list[Token]:
     whitespace: Pattern[str] = re.compile(r"\s+")
-    patterns: dict[str, Pattern[str]] = {
+    comment: Pattern[str] = re.compile(r"(//|#).*")
+    multiline_comment: Pattern[str] = re.compile(r"/\*[\s\S]*?\*/")
+
+    skip_patterns: list[Pattern[str]] = [whitespace, comment, multiline_comment]
+    token_patterns: dict[str, Pattern[str]] = {
         "identifier": re.compile(r"[a-zA-Z_][a-zA-Z0-9_]*"),
-        "int_literal": re.compile(r"\d+")
+        "int_literal": re.compile(r"\d+"),
+        "operator": re.compile(r"(==|!=|<=|>=)|[-+*/=<>]"),
+        "punctuation": re.compile(r"[(){},;]"),
     }
 
     i: int = 0
     tokens: list[Token] = []
     while i < len(source_code):
         start: int = i
-        match: Match[str] | None = whitespace.match(source_code, i)
-        if match:
-            i = match.end()
-        for pattern in patterns:
-            i = _extract_token(source_code, pattern, patterns[pattern], tokens, i)
+
+        for regex in skip_patterns:
+            i = _skip_pattern(source_code, regex, i)
+
+        for category in token_patterns.keys():
+            i = _extract_token(source_code, category, token_patterns[category], tokens, i)
+
         if i == start:
             raise SyntaxError(f"Unrecognized character: {source_code[i]}")
 
     return tokens
 
+def _skip_pattern(source_code: str, regex: Pattern[str],  position: int) -> int:
+    match: Match[str] | None = regex.match(source_code, position)
+    if match:
+        return match.end()
+    return position
 
 def _extract_token(source_code: str, category: str, regex: Pattern[str], tokens: list[Token], position: int) -> int:
     match: Match[str] | None = regex.match(source_code, position)
--- tests/tokenizer_test.py
+++ tests/tokenizer_test.py
@@ -31,21 +31,82 @@ class TestTokenizer(TestCase):
             Token("identifier", "when", self.L)
         ]
         code = "variableName \n\n\n name_of_variable   if   when"
+
         self.assertEqual(expect, tokenize(code))
 
-    def test_tokenizer_basics(self):
+    def test_tokenizer_operators(self):
+        operators = "+ - * / = == != < <= > >="
+
+        expect = []
+        for op in operators.split(" "):
+            expect.append(Token("operator", op, self.L))
+
+        self.assertEqual(expect, tokenize(operators))
+
+    def test_tokenizer_punctuation(self):
+        punctuation = "{ ) ( } , ;"
+
+        expect = []
+        for pun in punctuation.split(" "):
+            expect.append(Token("punctuation", pun, self.L))
+
+        self.assertEqual(expect, tokenize(punctuation))
+
+    def test_tokenizer_one_line_comments(self):
+        command = """
+            // this is a comment
+            if 3 // is also a comment
+            // we should only have 2 tokens
+            // for each 3 { == != = while
+        """
+
+        self.assertEqual(2, len(tokenize(command)))
+
+    def test_tokenizer_multiline_comments(self):
+        command = """
+            /* this comment spans
+            dlafj 5€£ 135€::lkjfd.s,f s,() @@@
+            if (3+3) != 6
+            */ 
+             
+            1 + 3 = 4 /* alsdfjkasdkf jasdlfjs f */\n
+            {2 /*() \n
+            */
+            }
+        """
+
+        # Expected tokens 1 + 3 = 4 { 2 }
+        self.assertEqual(8, len(tokenize(command)))
+
+    def test_tokenizer_combined_use(self):
         expect = [
             Token("identifier", "if", self.L),
+            Token("punctuation", "(", self.L),
             Token("int_literal", "3", self.L),
-            Token("identifier", "while", self.L)
+            Token("operator", "+", self.L),
+            Token("int_literal", "2", self.L),
+            Token("punctuation", ")", self.L),
+            Token("operator", "==", self.L),
+            Token("int_literal", "5", self.L),
+            Token("identifier", "x", self.L),
+            Token("operator", "=", self.L),
+            Token("int_literal", "2", self.L),
         ]
-        self.assertEqual(expect, tokenize("if 3\nwhile"))
+
+        command = """
+        // commentary
+        if (3 + 2) == 5\n
+            x = 2
+        """
+
+        self.assertEqual(expect, tokenize(command))
 
     def test_tokenizer_raises_exception_from_unrecognized_symbol(self):
         with self.assertRaises(SyntaxError):
             tokenize("while @  if True")
 
-    def test_token_defined_with_place_holder_location(self):
+    def test_token_defined_with_place_holder_location_by_default(self):
         self.location_patcher.stop()
         placeholder = Location("placeholder", 0, 0)
+
         self.assertEqual([Token("identifier", "variableName", placeholder)], tokenize("variableName"))
