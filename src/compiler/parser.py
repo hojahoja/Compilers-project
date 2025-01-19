d9b9f5780b05fc66bfb6e00243b601684bf1e774
hojahoja <5722145+hojahoja@users.noreply.github.com>
2025-01-19 22:43:13 +0200

    Parser example done up to precedence.

---
+++ src/compiler/parser.py
@@ -0,0 +1,72 @@
+import compiler.bast as ast
+from compiler.tokenizer import Token
+
+
+def parse(tokens: list[Token], left_ast: bool = True) -> ast.Expression:
+    pos: int = 0
+
+    def peek() -> Token:
+        if pos < len(tokens):
+            return tokens[pos]
+        else:
+            return Token(category="end", text="", location=tokens[-1].location)
+
+    def consume(expected: str | list[str] | None = None) -> Token:
+        nonlocal pos
+        token: Token = peek()
+
+        if isinstance(expected, str) and token.text != expected:
+            raise Exception(f'{token.location}: "{expected}"')
+        if isinstance(expected, list) and token.text not in expected:
+            comma_separated: str = ", ".join([f"{e}" for e in expected])
+            raise Exception(f"{token.location}: expected one of: '{comma_separated}'")
+
+        pos += 1
+        return token
+
+    def parse_int_literal() -> ast.Literal:
+        if peek().category != "int_literal":
+            raise Exception(f"{peek().location}: expected an integer literal")
+        token: Token = consume()
+        return ast.Literal(int(token.text))
+
+    def parse_identifier() -> ast.Identifier:
+        if peek().category != "identifier":
+            raise Exception(f"{peek().location}: expected an identifier")
+        token: Token = consume()
+        return ast.Identifier(token.text)
+
+    # TODO Precedence
+    def parse_term() -> ast.Expression:
+        if peek().category == "int_literal":
+            return parse_int_literal()
+        elif peek().category == "identifier":
+            return parse_identifier()
+        else:
+            raise Exception(f"{peek().location}: expected an integer literal or an identifier")
+
+    def parse_expression() -> ast.Expression:
+        left: ast.Expression = parse_term()
+
+        while peek().text in ["+", "-"]:
+            operator_token: Token = consume()
+            operator: str = operator_token.text
+
+            right: ast.Expression = parse_term()
+            left = ast.BinaryOp(left, operator, right)
+
+        return left
+
+    def parse_expression_right() -> ast.Expression:
+        left: ast.Expression = parse_term()
+
+        if peek().text in ["+", "-"]:
+            operator_token: Token = consume()
+            operator: str = operator_token.text
+
+            right: ast.Expression = parse_expression_right()
+            return ast.BinaryOp(left, operator, right)
+
+        return left
+
+    return parse_expression() if left_ast else parse_expression_right()
---
+++ tests/parser_test.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+
+import compiler.bast as ast
+from compiler.parser import parse
+from compiler.tokenizer import tokenize
+
+
+# mypy: ignore-errors
+class TestParser(TestCase):
+
+    def test_simple_binary_summation_into_ast_node(self):
+        tokens = tokenize("3 + 2")
+        result = parse(tokens)
+
+        expect = ast.BinaryOp(ast.Literal(3), "+", ast.Literal(2))
+        self.assertEqual(expect, result)
+
+    def test_simple_binary_summation_with_a_variable_into_ast_node(self):
+        tokens = tokenize("3 - a")
+        result = parse(tokens)
+
+        expect = ast.BinaryOp(ast.Literal(3), "-", ast.Identifier("a"))
+        self.assertEqual(expect, result)
+
+    def test_binary_expression_with_multiple_variables_and_literals_into_ast_node(self):
+        tokens = tokenize("2 - variable + 2 + x")
+        result = parse(tokens)
+
+        minus = ast.BinaryOp(ast.Literal(2), "-", ast.Identifier("variable"))
+        plus2 = ast.BinaryOp(minus, "+", ast.Literal(2))
+        expect = ast.BinaryOp(plus2, "+", ast.Identifier("x"))
+
+        self.assertEqual(expect, result)
+
+
+    def test_binary_parse_expression_right_associative(self):
+        tokens = tokenize("2 - variable + 2 + x")
+        result = parse(tokens, left_ast=False)
+
+        plus2 = ast.BinaryOp(ast.Literal(2), "+", ast.Identifier("x"))
+        plus1 = ast.BinaryOp(ast.Identifier("variable"), "+", plus2)
+        expect = ast.BinaryOp(ast.Literal(2), "-", plus1)
+
+        self.assertEqual(expect, result)
